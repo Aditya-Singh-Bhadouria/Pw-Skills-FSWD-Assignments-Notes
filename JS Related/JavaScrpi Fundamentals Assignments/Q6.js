b79624f504319911748f815d61fa83fe0118db4b
Adity Singh Bhadouria <bhadouriaaditya63@gmail.com>
2023-08-03 22:51:14 +0530

    Q6

---
+++ JS Related/JavaScrpi Fundamentals Assignments/Q6.js
@@ -0,0 +1,15 @@
+let name = "Mithun";
+let email = "mithun.s@pw.live";
+let age = "21";
+
+let type_email = typeof email;
+let type_age = typeof age;
+
+
+if (type_email != "string") {
+    console.log("Email should be a string")
+}
+
+if (type_age != "number") {
+    console.log("Age should be a Number" )
+}
