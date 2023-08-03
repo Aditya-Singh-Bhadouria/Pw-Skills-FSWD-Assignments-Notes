92f7176cb702f3ab0017045a1a8c2c30262a06f5
Adity Singh Bhadouria <bhadouriaaditya63@gmail.com>
2023-08-03 22:50:22 +0530

    Q4

---
+++ JS Related/JavaScrpi Fundamentals Assignments/Q4.js
@@ -0,0 +1,9 @@
+let numberOfChild = 2;
+let numberOfAdults = 1;
+let numberOfSeniors = 1;
+
+let P_C = 100;
+let P_A = 150;
+let P_S = 120;
+
+console.log("The total ticket price is " + ((numberOfChild * P_C) + (numberOfAdults * P_A) + (numberOfSeniors * P_S)));
