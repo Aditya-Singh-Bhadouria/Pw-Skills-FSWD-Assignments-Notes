39a0aa6b38e6fc3a1201c656989a5f564424b9dd
Adity Singh Bhadouria <bhadouriaaditya63@gmail.com>
2023-08-03 22:50:35 +0530

    Q5

---
+++ JS Related/JavaScrpi Fundamentals Assignments/Q5.js
@@ -0,0 +1,11 @@
+let package = "standard";
+
+if (package == "standard") {
+    console.log("standard might take 3 - 5 days");
+}else if (package == "express") {
+    console.log("express might take 1 - 2 days");
+}else if (package == "overnight") {
+    console.log("overnight - would be delivered next day");
+}else{
+    console.log("chose form -> standard , express , overnights");
+}
